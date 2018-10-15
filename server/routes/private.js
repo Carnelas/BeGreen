be4020641dda083a40325626001a0cc46521c68f
Carnelas <carnelas@gmail.com>
2018-10-15 20:35:30 +0200

    solucionado problema qty en front

--- server/routes/private.js
+++ server/routes/private.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const Item = require('../models/Item');
+const User = require('../models/User')
 
 
 //preguntar cómo hacer que coja él solo el seller (desde el front)
@@ -12,20 +13,20 @@ router.post('/item', (req, res, next) => {
         seller,
         price,
         qty
-    }).save().then(() => {res.status(200).json({status: 'objeto guardado'})})
-    .catch(e => next(e));
+    }).save().then(() => { res.status(200).json({ status: 'objeto guardado' }) })
+        .catch(e => next(e));
 
 })
 
 //recoger articulos desde la base de datos
-router.get('/item', (req,res,next) => {
+router.get('/item', (req, res, next) => {
     Item.find()
-    .then(data => res.status(200).json(data))
-    .catch(e => next(e))
+        .then(data => res.status(200).json(data))
+        .catch(e => next(e))
 }
 )
 // borrar los objetos por ID
-/* router.delete('/:id',(req,res,next) => {
+/* router.delete('/item/:id',(req,res,next) => {
     const {id} = req.params;
     Model.findByIdAndRemove(id)
         .then( obj => {
@@ -42,25 +43,11 @@ router.get('/item', (req,res,next) => {
     res.status(500).json({ message: err.message });
 }) */
 
-router.get('/items/:id', (req, res, next)=>{
-
-    if(!mongoose.Types.ObjectId.isValid(req.params.id)) {
-      res.status(400).json({ message: 'Specified id is not valid' });
-      return;
-    }
-  
-    // our projects have array of tasks' ids and 
-    // we can use .populate() method to get the whole task objects
-    //                                   ^
-    //                                   |
-    //                                   |
-    Project.findById(req.params.id).populate('tasks')
-      .then(response => {
-        res.json(response);
-      })
-      .catch(err => {
-        res.json(err);
-      })
-  })
+//Gabi socorro
+router.get('/user/:_id', (req, res, next) => {
+    User.findById({_id: req.params._id})
+    .then(data => res.status(200).json(data))
+    .catch(e => next(e))
+  }) 
 
 module.exports = router;
